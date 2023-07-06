7b26ac41c7ed7d4420bb87fa7ea859a6e8219c90
russmaster76 <97500904+russmaster76@users.noreply.github.com>
2023-07-06 15:55:31 -0700

    slight update

--- website/src/components/homePage/homePage.js
+++ website/src/components/homePage/homePage.js
@@ -2,16 +2,38 @@ import './HomePage.css'
 import axios from 'axios'
 
 function HomePage() {
-  var searchBarContent = ""
+  var  searchBarContent = ""
+
+  function onSearchBarChange(e) {
+    e.preventDefault()
+    searchBarContent = e.target.value
+    console.log(e.target.value)
+  }
+
+  function onSearchButtonClick(e) {
+    console.log(searchBarContent)
+  }
+
   function searchSongs() {
     let queryObj = {
-
+      searchQuery: searchBarContent
     }
+
+    axios.post('http://127.0.0.1:5000/search', queryObj)
+    .then(
+      response => response.data
+    )
+    .then(
+      
+    )
   }
   return (
     <div className="app">
+      <h1 className="headerTitle">Learn Languages From Music</h1>
         <div className="search-bar-container">
-          <input type="text" className="search-input" placeholder="Search" />
+          <input type="text" className="search-input" 
+          placeholder="Enter Song Title Here" id="searchBar" onChange={onSearchBarChange}/>
+          <button className="searchButton" onClick={onSearchButtonClick}>Search</button>
         </div>
         {/* Other components and content */}
     </div>
